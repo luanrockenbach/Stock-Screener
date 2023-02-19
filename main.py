64751bb3d720eac7e3a029c4dfed88c623e78e22
Luan Rockenbach <luan0rockenbach@gmail.com>
2023-02-19 12:13:55 -0300

    Graph Theme edition

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import plotly.express as px
 from dash import Dash, html, dcc
 
 
+ticket = input(str("\n - Digite qual ação você gostaria de analisar: "))
+
+
 def show_dashboard(filtered_stocks):
     # criar um gráfico de dispersão com as ações filtradas
 
-    app = Dash(__name__)
+    app = Dash(__name__, title='Djin', )
 
-    fig = px.line(filtered_stocks, x='Date', y='Open')
+    fig = px.line(filtered_stocks, x='Date', y='Open', template='plotly_dark', title=str(ticket))
 
     app.layout = html.Div(children=[
-        html.H1(children='Hello Dash'),
+        html.H1(children='Djin', style={'textAlign': 'center', 'color': 'blue', 'fontSize': 50}),
 
         html.Div(children='''
             Dash: A web application framework for your data.
@@ -28,9 +31,12 @@ def show_dashboard(filtered_stocks):
         app.run_server(debug=True)
 
 
-ticket = input(str("\n - Digite qual ação você gostaria de analisar: "))
-ticker = yfinance.Ticker(ticket+".SA")
+def get_stock(ticker):
+    ticker = yfinance.Ticker(ticket+".SA")
 
-stock_data = pd.DataFrame(ticker.history(period="1mo", interval="1d"))
-stock_data.reset_index(inplace=True)
-show_dashboard(stock_data)
+    stock_data = pd.DataFrame(ticker.history(period="1mo", interval="1d"))
+    stock_data.reset_index(inplace=True)
+    return stock_data
+
+
+show_dashboard(get_stock(ticket))
